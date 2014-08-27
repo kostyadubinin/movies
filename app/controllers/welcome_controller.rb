1bc3c211d0ce960c08d0337ae97f3ef03c72e21c
Kostya Dubinin <dubininkostya@gmail.com>
2014-08-28 00:04:23 +0300

    Update the main page to show popular movies

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,5 @@
 class WelcomeController < ApplicationController
+  def index
+    @movies = Tmdb::Movie.popular
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def poster_url(movie)
+    poster_size = 'w342'
+    configuration.base_url + poster_size + movie.poster_path
+  end
+
+  private
+
+  # TODO: Cache this. From the documentation:
+  #
+  #   It is recommended you cache this data within your application
+  #   and check for updates every few days.
+  #
+  def configuration
+    @configuration ||= Tmdb::Configuration.new
+  end
 end
