daee17ece9ec7e40e2383b60b978296a5e4c0b23
Kostya Dubinin <dubininkostya@gmail.com>
2014-10-21 18:54:09 +0300

    Create the movie page (e.g. /movies/1)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,6 +4,15 @@ module ApplicationHelper
     configuration.base_url + poster_size + movie.poster_path
   end
 
+  def backdrop_url(movie)
+    backdrop_size = 'w1280'
+    configuration.base_url + backdrop_size + movie.backdrop_path
+  end
+
+  def imdb_url(imdb_id)
+    "http://www.imdb.com/title/#{imdb_id}/"
+  end
+
   private
 
   # TODO: Cache this. From the documentation:
