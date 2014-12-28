3510b7b881ed58acc568ba94b846fdd8cb16a4e5
Kostya Dubinin <dubininkostya@gmail.com>
2014-12-28 23:43:18 +0200

    Add support for flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,4 +13,13 @@ module ApplicationHelper
   def configuration
     @configuration ||= Tmdb::Configuration.new
   end
+
+  def flash_class(level)
+    case level
+    when 'success' then 'alert alert-success'
+    when 'info'    then 'alert alert-info'
+    when 'warning' then 'alert alert-warning'
+    when 'danger'  then 'alert alert-danger'
+    end
+  end
 end
