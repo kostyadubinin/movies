96b20ec50db354f74517dcfb33bc55b122032af5
Kostya Dubinin <dubininkostya@gmail.com>
2015-01-02 01:59:37 +0200

    Add movie filters

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,22 @@
 class MoviesController < ApplicationController
-  def index
+  def popular
     @movies = Tmdb::Movie.popular || []
+    render 'index'
+  end
+
+  def top_rated
+    @movies = Tmdb::Movie.top_rated || []
+    render 'index'
+  end
+
+  def now_playing
+    @movies = Tmdb::Movie.now_playing || []
+    render 'index'
+  end
+
+  def upcoming
+    @movies = Tmdb::Movie.upcoming || []
+    render 'index'
   end
 
   def show
