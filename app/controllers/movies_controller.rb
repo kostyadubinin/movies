c68d1d788d22ea71e74e9c1286da8d3541c369e9
Kostya Dubinin <dubininkostya@gmail.com>
2015-01-03 01:50:40 +0200

    Add Movie model

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,26 +1,25 @@
 class MoviesController < ApplicationController
   def popular
-    @movies = Tmdb::Movie.popular
+    @movies = Movie.popular
     render 'index'
   end
 
   def top_rated
-    @movies = Tmdb::Movie.top_rated
+    @movies = Movie.top_rated
     render 'index'
   end
 
   def now_playing
-    @movies = Tmdb::Movie.now_playing
+    @movies = Movie.now_playing
     render 'index'
   end
 
   def upcoming
-    @movies = Tmdb::Movie.upcoming
+    @movies = Movie.upcoming
     render 'index'
   end
 
   def show
-    # TODO: Handle invalid ids.
-    @movie = Tmdb::Movie.detail(params[:id], append_to_response: 'trailers,credits')
+    @movie = Movie.find(params[:id], append_to_response: 'trailers,credits')
   end
 end
