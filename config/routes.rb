ada439ab694896b783eb0cd29b628ac906f0fc13
Kostya Dubinin <dubininkostya@gmail.com>
2015-01-02 19:54:18 +0200

    Refactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  resources :movies, only: %w(show) do
+  resources :movies, only: :show do
     collection do
       get 'popular'
       get 'top_rated'
