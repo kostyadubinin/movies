e195f15cbe2cd92ff9caeb575d41a8e0e90a7692
Kostya Dubinin <dubininkostya@gmail.com>
2015-02-07 01:31:35 +0200

    Refactor faraday initializer

--- config/initializers/faraday.rb
+++ config/initializers/faraday.rb
@@ -1,7 +1,5 @@
-tmdb_api_url = 'https://api.themoviedb.org/3/'
-params = { api_key: ENV["TMDB_API_KEY"] }
-
-$tmdb = Faraday.new(url: tmdb_api_url, params: params) do |faraday|
+$tmdb = Faraday.new(url: 'https://api.themoviedb.org/3/') do |faraday|
+  faraday.params[:api_key] = ENV["TMDB_API_KEY"]
   faraday.request  :url_encoded             # form-encode POST params
   faraday.response :json, :content_type => /\bjson$/
   faraday.response :logger                  # log requests to STDOUT
