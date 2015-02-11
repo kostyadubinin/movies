918f7c81242c3cda7e0afac9f7b13a0fc080a23f
Kostya Dubinin <dubininkostya@gmail.com>
2015-02-11 22:40:46 +0200

    Simplify movie decorator

--- app/decorators/movie_decorator.rb
+++ app/decorators/movie_decorator.rb
@@ -1,81 +1,67 @@
 class MovieDecorator < Draper::Decorator
   def id
-    handle_none object["id"] do
-      object["id"]
-    end
+    object["id"]
   end
 
   def title
-    handle_none object["title"] do
-      object["title"]
-    end
+    object["title"]
   end
 
   def overview
-    handle_none object["overview"] do
-      object["overview"]
-    end
+    object["overview"]
   end
 
   def tagline
-    handle_none object["tagline"] do
-      object["tagline"]
-    end
+    object["tagline"]
   end
 
-  def belongs_to_collection
-    handle_none object["belongs_to_collection"] do
-      CollectionDecorator.decorate(object["belongs_to_collection"])
-    end
+  def poster_path
+    object["poster_path"]
   end
 
   def genres
     GenreDecorator.decorate_collection(object["genres"])
   end
 
+  def youtube_trailers
+    TrailerDecorator.decorate_collection(object["trailers"]["youtube"])
+  end
+
+  def belongs_to_collection
+    if object["belongs_to_collection"].present?
+      CollectionDecorator.decorate(object["belongs_to_collection"])
+    end
+  end
+
   def release_date
-    handle_none object["release_date"] do
+    if object["release_date"].present?
       Time.parse(object["release_date"]).strftime("%B %Y")
     end
   end
 
   def budget
-    handle_none object["budget"] do
+    unless object["budget"].to_i.zero?
       h.number_to_currency(object["budget"], precision: 0)
     end
   end
 
   def runtime
-    handle_none object["runtime"] do
+    unless object["runtime"].to_i.zero?
       object["runtime"]
     end
   end
 
   def vote_average
-    handle_none object["vote_average"] do
+    unless object["vote_average"].to_f.zero?
       object["vote_average"]
     end
   end
 
   def poster_url(size: 'w342')
-    if object["poster_path"]
-      "#{h.configuration.base_url}#{size}#{object["poster_path"]}"
+    if poster_path.present?
+      "#{h.configuration.base_url}#{size}#{poster_path}"
     else
       "http://dummyimage.com/342x513/d9d9d9/000000.png&text=N/A"
-    end
-  end
-
-  def youtube_trailers
-    TrailerDecorator.decorate_collection(object["trailers"]["youtube"])
-  end
-
-  private
-
-  def handle_none(value)
-    if value.present? && value != 0
-      yield
-    else
-      nil
     end
   end
 end
