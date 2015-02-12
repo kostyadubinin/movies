c311254cfe97f8e14b3e5f8b1a08f56d44f63331
Kostya Dubinin <dubininkostya@gmail.com>
2015-02-12 23:30:39 +0200

    Refactor Bootstrap responsive column resets

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,25 @@
 module ApplicationHelper
+  # Responsive column resets for Bootstrap. See:
+  # http://getbootstrap.com/css/#grid
+  #
+  # index        - column number
+  # columns_spec - number of columns per grid tier, i.g { xs: 2, sm: 3 }
+  #
+  # Examples
+  #
+  #   responsive_columns_fix(4, xs: 2, sm: 3, md: 4, lg: 6)
+  #   # => "<div class=\"clearfix visible-xs-block visible-md-block\"></div>"
+  #
+  #   responsive_columns_fix(1, xs: 2, sm: 3, md: 4, lg: 6)
+  #   # => nil
+  #
+  def responsive_columns_fix(index, columns_spec)
+    tiers = columns_spec.map { |tier, width| index % width == 0 ? tier : nil }
+    tiers = tiers.compact.map { |tier| "visible-#{tier}-block" }
+
+    content_tag(:div, nil, class: "clearfix #{tiers.join(' ')}") if tiers.any?
+  end
+
   def youtube_url(source)
     "//www.youtube.com/embed/#{source}"
   end
