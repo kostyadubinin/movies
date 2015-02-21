e88af7722de17841f1daf4f73666c556d2d68365
Kostya Dubinin <dubininkostya@gmail.com>
2015-02-21 21:05:39 +0200

    Configure Faraday logger

--- config/initializers/faraday.rb
+++ config/initializers/faraday.rb
@@ -2,6 +2,9 @@ $tmdb = Faraday.new(url: "https://api.themoviedb.org/3/") do |faraday|
   faraday.params[:api_key] = ENV["TMDB_API_KEY"]
   faraday.request :url_encoded # form-encode POST params
   faraday.response :json, content_type: /\bjson$/
-  faraday.response :logger # log requests to STDOUT
+  faraday.response :logger, ActiveSupport::TaggedLogging.new(
+    Logger.new("log/faraday.log")
+  )
+
   faraday.adapter Faraday.default_adapter # make requests with Net::HTTP
 end
