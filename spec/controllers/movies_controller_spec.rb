54a0836d6231e8fb3426917bd2208f2070e2789e
Kostya Dubinin <dubininkostya@gmail.com>
2015-02-21 14:18:31 +0200

    Configure VCR to skip api_key

--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 describe MoviesController do
   describe "GET popular" do
     it "assigns @movies" do
-      VCR.use_cassette("movies_popular") do
+      VCR.use_cassette("movies_popular", match_requests_on: [VCR.request_matchers.uri_without_param(:api_key)]) do
         get :popular
       end
 
