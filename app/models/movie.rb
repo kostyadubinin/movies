cfa7bc3199347e51a3c817965c65ac7cfc9fc42b
Kostya Dubinin <dubininkostya@gamil.com>
2015-03-20 00:00:44 +0200

    Add description for some Movie methods

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -13,24 +13,29 @@ class Movie
     @body = body
   end
 
+  # Returns false or true.
   def adult
     body["adult"]
   end
 
+  # Returns nonempty String or nil.
   def backdrop_path
     body["backdrop_path"]
   end
 
+  # Returns Hash or nil.
   def belongs_to_collection
     if body["belongs_to_collection"].present?
       Collection.new(body["belongs_to_collection"])
     end
   end
 
+  # Returns String.
   def title
     body["title"]
   end
 
+  # Returns Array of Hashes or empty Array.
   def genres
     body["genres"].map { |g| Genre.new(g) }
   end
