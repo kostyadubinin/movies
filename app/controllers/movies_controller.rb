ed882f523e14cd85042f2e8bf6e2155e7adce74c
Kostya Dubinin <dubininkostya@gamil.com>
2015-04-26 14:24:53 +0300

    Refactor MoviesController#index

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,10 +2,10 @@ class MoviesController < ApplicationController
   def index
     @movies = MovieDecorator.decorate_collection(
       case params[:order]
+      when nil           then Movie.popular
       when "top_rated"   then Movie.top_rated
       when "upcoming"    then Movie.upcoming
       when "now_playing" then Movie.now_playing
-      else Movie.popular
       end
     )
   end
