3cab17245dfc03481bb7d7388f54452d8d5808e1
Kostya Dubinin <dubininkostya@gamil.com>
2015-04-26 23:55:48 +0300

    Align lines

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,9 @@ class User < ActiveRecord::Base
   def self.from_omniauth(auth)
     user = where(provider: auth.provider, uid: auth.uid).first_or_initialize
 
-    user.email = auth.info.email
+    user.email      = auth.info.email
     user.first_name = auth.info.first_name
-    user.image = auth.info.image
+    user.image      = auth.info.image
     user.save
 
     user
