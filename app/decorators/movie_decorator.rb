45c92692bd01a544c11df9591f8dfd6ffae66abc
Kostya Dubinin <dubininkostya@gamil.com>
2015-04-26 17:53:43 +0300

    Specify width for movie posters

--- app/decorators/movie_decorator.rb
+++ app/decorators/movie_decorator.rb
@@ -2,10 +2,14 @@ class MovieDecorator < Draper::Decorator
   delegate_all
 
   # TODO: Use TMDb Configuration.
-  def poster_url
-    base_url = "http://image.tmdb.org/t/p/"
-    size = "w342"
+  POSTER_SIZE = 342
+  BASE_URL = "http://image.tmdb.org/t/p/"
 
-    "#{base_url}#{size}#{object.poster_path}"
+  def poster_url
+    "#{BASE_URL}w#{POSTER_SIZE}#{object.poster_path}"
+  end
+
+  def poster_width
+    POSTER_SIZE
   end
 end
