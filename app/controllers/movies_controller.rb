6d1edaa4a44292ed7a3048e1906a59d0dcecf44d
Kostya Dubinin <dubininkostya@gmail.com>
2015-06-11 21:07:31 +0300

    Make page url parameter available for using

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,12 +1,19 @@
 class MoviesController < ApplicationController
+  # TODO: validate params[:page]
   def index
     @movies = MovieDecorator.decorate_collection(
-      case params[:order]
-      when nil           then Movie.popular
-      when "top_rated"   then Movie.top_rated
-      when "upcoming"    then Movie.upcoming
-      when "now_playing" then Movie.now_playing
-      end
+      movies(order: params[:order]).page(params[:page])
     )
+  end
+
+  private
+
+  def movies(order:)
+    case order
+    when nil           then Movie.popular
+    when "top_rated"   then Movie.top_rated
+    when "upcoming"    then Movie.upcoming
+    when "now_playing" then Movie.now_playing
+    end
   end
 end
