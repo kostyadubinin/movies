4dc5fd59cae919dad1bb9d9df1958d9f89d81144
Kostya Dubinin <dubininkostya@gmail.com>
2015-06-18 23:40:31 +0300

    Remove order nav

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
 
-  # TODO: These get routes doesn't look very good. Do something.
-  get ":order/page/:page", to: "movies#index", order: /top_rated|upcoming|now_playing/
-  get ":order", to: "movies#index", order: /top_rated|upcoming|now_playing/, as: :ordered_movies
-
   resources :movies, path: "/", only: %w(index) do
     get 'page/:page', action: :index, on: :collection
   end
